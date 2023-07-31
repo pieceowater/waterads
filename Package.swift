3751311121c8100eb2e2ecde3de21cd86e759f77
pieceowater <yury.lvn@mail.ru>
2023-07-31 13:33:27 +0600

    init test

--- Package.swift
+++ Package.swift
@@ -1,26 +1,30 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 
 import PackageDescription
 
 let package = Package(
     name: "waterads",
+    platforms: [
+        .iOS(.v15),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "waterads",
             targets: ["waterads"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "10.7.0"),
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-SDK-iOS.git", from: "11.1.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "waterads",
-            dependencies: []),
+            path: "Sources",
+            resources: [
+                .process("Images/Media.xcassets")
+            ]
+        ),
         .testTarget(
             name: "wateradsTests",
             dependencies: ["waterads"]),
